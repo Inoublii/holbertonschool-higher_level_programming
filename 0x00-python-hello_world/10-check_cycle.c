7db2d3560b9aa543a5f79b838bbade0f9e99205b
inoublii <inoublii@gmail.com>
2019-12-09 21:38:34 +0100

    advanced

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * check_cycle - check if list has a cycle
+ * @head: pointer to list to be freed
+ * Return: 0 if has no cycle or 1 if has cycle
+ */
+int check_cycle(listint_t *list)
+{
+	
