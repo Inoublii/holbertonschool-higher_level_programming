a07b4c470370cbe3f63cc064780fa12d2b5040b3
inoublii <inoublii@gmail.com>
2020-01-08 20:03:12 +0100

    push me

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    a = 0
+    i = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            a = a + 1
+        except IndexError:
+            break
+    print()
+    return (a)
