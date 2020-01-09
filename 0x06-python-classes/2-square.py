fba60b5e40d978668165bbd6f7b01ebc31d18247
inoubli <inoublii@gmail.com>
2020-01-09 22:05:53 +0000

    works

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 class Square:
-	def __init__(self,size=0):
-		if not type(size) == int:
-			raise TypeError("size must be an integer")
-		if size < 0:
-			raise ValueError("size must be >= 0")
+    def __init__(self, size=0):
+        if not type(size) == int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+__doc__ = "Square class"
+__dic__ = "__init__"
