b4d241da0cbba2f5b6234f3eed097485e276e126
inoubli <inoublii@gmail.com>
2020-01-09 22:08:03 +0000

    works

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
+__doc__ = "__init__ func"
         if not type(size) == int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
 __doc__ = "Square class"
-__doc__ = "__init__ func"
 __dic__ = "__init__"
