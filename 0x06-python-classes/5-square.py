76fd1d8d5417d0a0f01e0386753e2ef953fca082
inoubli <inoublii@gmail.com>
2020-01-09 23:17:17 +0000

    fixed whitespaces

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+        if not type(size) == int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return(self.__size * self.__size)
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not type(value) == int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+            return
+
+        for i in range(self.size):
+            for x in range(self.size):
+                print('#', end='')
+            print()
