3752d0b79cbef54c7f3b333bb6d5c53391ad7729
inoubli <inoublii@gmail.com>
2020-01-09 22:58:02 +0000

    fixed

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,6 +6,8 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
+
         return(self.__size * self.__size)
 __doc__ = " Square class"
