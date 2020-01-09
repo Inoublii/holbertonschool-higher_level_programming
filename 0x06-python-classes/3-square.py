ec10f1d32db614ee5ef9d7730253416feb5fa77b
inoubli <inoublii@gmail.com>
2020-01-09 22:55:06 +0000

    fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,5 +8,4 @@ class Square:
             raise ValueError("size must be >= 0")
     def area(self):
         return(self.__size * self.__size)
-__doc = "Square class"
-__square_size = {}
+__doc__ = " Square class"
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 class Square:
-	"""hello"""
-	def __init__(self, size=0):
-		self.__size = size
-		if not type(size) == int:
-			raise TypeError("size must be an integer")
-		if size < 0:
-			raise ValueError("size must be >= 0")
-	def area(self):
-		return(self.__size * self.__size)
-	@property
-	def size(self):
-		return(self.__size)
-	@size.setter
-	def size(self, value):
-		self.__size = value
-		if not type(value) == int:
-			raise TypeError("size must be an integer")
-		if value < 0:
-			raise ValueError("size must be >= 0")
+    def __init__(self, size = 0):
+        self.__size = size
+        if not type(size) == int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
 
+    def area(self):
+        return(self.__size * self.__size)
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not type(value) == int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
