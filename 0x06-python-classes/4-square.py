3fbc0dcbd922739614cbc324faff7e872ac33a0b
inoubli <inoublii@gmail.com>
2020-01-10 10:48:10 +0000

    fixed whitespace

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         self.__size = size
         if not type(size) == int:
             raise TypeError("size must be an integer")
@@ -21,4 +21,4 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-__doc__ = "Square class" 
+__doc__ = "Square class"
