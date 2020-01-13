ecd127237cea82605c2047750aba0b76fe8ade96
inoublii <inoublii@gmail.com>
2020-01-13 23:28:43 +0100

    added dock

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
+	"""Divides matrix by div.
+    vs:
+        matrix: dictionairy
+        div: number
+    Returns:
+        list: Dictionairy
+    Raises:
+        TypeError: If matrix is not list containing int or float.
+        TypeError: If lists are not same size.
+        TypeError: If div is not int or float.
+        ZeroDivisionError: If div is zero.
+    """
 	new = []
 	for i in range(matrix):
 		new += matrix[i] / div
 	return new
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
