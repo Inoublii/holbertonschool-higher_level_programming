e61fc65cb8ef94436c711d4f2c13411d888ac62b
inoublii <inoublii@gmail.com>
2020-01-14 00:02:28 +0100

    added docs

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
+    """text_indentation"""
 def text_indentation(text):
+	"""add 2 new lines after '.?:'
+    str:
+        text: str
+    Raises:
+        TypeError: If text is not str
+    """
 	if type(text) != str:
 		raise TypeError("text must be a string")
-	l = "\n"
-	for i in text:
-		if i == "." or i == "?" or i == ":":
-			i += l 
-		print(i, end="")
+	l = ".?:"
+	for x in l:
+		text = (x + "\n\n").join([a.strip(" ") for a in text.split(x)])
+	print(text, end="")
