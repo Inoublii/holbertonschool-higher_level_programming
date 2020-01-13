352a5df71f7692eab559f43893659a2de3674d54
inoublii <inoublii@gmail.com>
2020-01-13 23:37:27 +0100

    added doc

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
+"""print_square"""
 def print_square(size):
+	"""printing a square with #s
+    int:
+        size: size of the square
+    Raises:
+        TypeError: If size is not int
+        ValueError: If size < 0
+	"""
 	if type(size) is not int and size < 0:
 		raise TypeError("size must be an integer")
 	if size < 0:
