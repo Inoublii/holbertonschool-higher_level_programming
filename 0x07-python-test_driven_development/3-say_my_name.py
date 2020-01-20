6835f4dccd9f403997a72f99baf59f41ad671d38
inoublii <inoublii@gmail.com>
2020-01-20 22:03:38 +0100

    fixed tests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """Module for say_my_name ."""
+
+
 def say_my_name(first_name, last_name=""):
- """printing first and last name
-  str:
-   first_name: first name
-   last_name: last name
-Raises:
+    """printing first and last name
+    str:
+    first_name: first name
+    last_name: last name
+    Raises:
      TypeError: If first_name or last_name are not strings.
-  """
-	if type(first_name) != str:
-		raise TypeError("first_name must be a string")
-	if type(last_name) != str:
-		raise TypeError("las_name must be a string")
-	print("My name is", first_name, last_name)
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("las_name must be a string")
+    print("My name is", first_name, last_name)
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/3-say_my_name.txt")
