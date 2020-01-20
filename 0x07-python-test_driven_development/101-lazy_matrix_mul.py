9ff45ff8d4fa461ede6d6119141139bebb299fc7
inoublii <inoublii@gmail.com>
2020-01-20 22:35:44 +0100

    fix

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+01-lazy_matrix_mulmodule.
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    lazy_matrix_mul
+    """
+    return np.matmul(m_a, m_b)
