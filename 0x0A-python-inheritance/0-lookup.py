6eca4f06a30c5b19bdf6b888c7a9acbbe5a31a11
inoublii <inoublii@gmail.com>
2020-01-21 22:19:45 +0100

    first commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def lookup(obj):
+    """
+    function that returns the list of available attributes
+    and methods of an object
+    args:
+    obj: objet
+    RETURN: list
+    """
     return dir(obj)
