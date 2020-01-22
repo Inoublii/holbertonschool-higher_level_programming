3ca64ebaf2be36b810ab109cc66b41cdc4838576
inoublii <inoublii@gmail.com>
2020-01-23 00:00:45 +0100

    fixed

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 filename = "add_item.json"
