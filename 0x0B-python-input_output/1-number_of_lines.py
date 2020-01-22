0ba4a3ef77c1c9ab85c26a64f61c5a22df208783
inoublii <inoublii@gmail.com>
2020-01-23 00:02:15 +0100

    fixed

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -2,7 +2,7 @@
 def number_of_lines(filename=""):
     """ counts lines"""
     line = 0
-    with open(filename, encoding='utf-8') as FILE:
-        for x in File:
+    with open(filename, encoding='utf-8') as f:
+        for x in f:
             line = line + 1
     return line
