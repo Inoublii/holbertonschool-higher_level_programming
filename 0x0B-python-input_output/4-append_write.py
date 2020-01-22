ebf069325530440e8118a3e05ea56528dade2ef1
inoublii <inoublii@gmail.com>
2020-01-22 23:38:37 +0100

    fixed

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def append_write(filename="", text=""):
-    """ append to a file"""
+    """ append to a file
+    """
     with open(filename, 'a', encoding='utf-8') as x:
         return(x.write(text))
