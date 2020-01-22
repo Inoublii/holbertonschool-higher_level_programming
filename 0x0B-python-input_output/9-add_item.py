9377723e6cb2bc82607367cbe6f8f176a41f22c4
inoublii <inoublii@gmail.com>
2020-01-23 00:29:27 +0100

    fixed whitespaces

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -10,7 +10,7 @@ try:
     s = load_from_json_file(filename)
 except:
     with open(filename, 'w', encoding='utf_8') as x:
-        x.write('')
+        x.write("")
         s = []
-s += argv[1:]
+s += sys.argv[1:]
 save_to_json_file(s, filename)
