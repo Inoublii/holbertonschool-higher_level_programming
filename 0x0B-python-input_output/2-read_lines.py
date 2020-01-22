9fc052338cbdb01aa3539b01c6a8031e70173d84
inoublii <inoublii@gmail.com>
2020-01-23 00:25:27 +0100

    fixed whitespaces

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -5,13 +5,14 @@ def read_lines(filename="", nb_lines=0):
             filename
             nb_lines: number of lines
     """
-    with open(filename,"w", encoding='utf-8') as x:
+    with open(filename, encoding='utf-8') as x:
         if nb_lines <= 0:
             print(x.read(), end="")
         else:
             i = 0
             for line in x:
                 i += 1
+                print(line, end='')
                 if i == nb_lines:
                     break
-                print(line, end='')
+
