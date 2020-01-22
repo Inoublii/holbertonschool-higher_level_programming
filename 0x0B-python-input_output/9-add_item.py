e2a920d76e449aabb67f280f8e46ab8e69dfc37d
inoublii <inoublii@gmail.com>
2020-01-23 00:17:22 +0100

    fixed whitespaces

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -8,9 +8,8 @@ adds arguments to a list then saves to file
 """
 try:
     jsonlist = load_from_json_file(filename)
-except:
-    with open(filename, 'w', encoding='utf_8') as x:
-        x.write('')
+except Exception:
+
         jsonfile = []
 for s in argv[1:]:
     jsonlist.append(s)
