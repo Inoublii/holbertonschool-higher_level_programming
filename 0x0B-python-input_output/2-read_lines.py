de7e82ed9da64e815c14399bea969cfd5bf5c472
inoublii <inoublii@gmail.com>
2020-01-22 23:29:52 +0100

    Commir

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -8,9 +8,9 @@ def read_lines(filename="", nb_lines=0):
 i = 0
 with open(filename, encoding='utf-8') as x:
     if nb_lines <= 0:
-        print(f.read(), end='')
+        print(x.read(), end='')
     else:
-        for line in f:
+        for line in x:
             i += 1
             if i == nb_lines:
                 break
