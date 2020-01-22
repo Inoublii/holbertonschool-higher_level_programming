38e5bdf57730a96004bc31c35849b79bb58c58a3
inoublii <inoublii@gmail.com>
2020-01-22 23:43:41 +0100

    commit

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def save_to_json_file(my_obj, filename):
     import json
-    with open(filename, "s", encoding="utf-8") as x:
+    with open(filename, "w", encoding="utf-8") as x:
         json.dump(my_obj, x)
