f34f77127fbf51051a5a798836d7f0df20431a95
inoublii <inoublii@gmail.com>
2020-01-28 22:11:46 +0100

    fixed id

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -61,7 +61,7 @@ class Rectangle(Base):
             """dictionairy"""
             dic = {
                 "x": self.x, "width": self.width, "id": self.id,
-                "heeight": self.__height, "y": self.y,
+                "height": self.__height, "y": self.y,
             }
             return (dic)
 
