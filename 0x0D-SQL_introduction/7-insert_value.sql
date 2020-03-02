ea4dff64b68404e71640b08c05419d0d0882af15
inoublii <inoublii@gmail.com>
2020-03-02 20:53:36 +0100

    fx

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- inserts a new row in the table first_table
 -- database name will be passed as an argument of the mysql command
 
-INSERT INTO 'first_table' ('id', 'name') VALUES(89, 'Holberton School');
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Holberton School');
