b7d0440f719da675a63ed3362ec314f94e478fad
inoublii <inoublii@gmail.com>
2020-03-26 13:26:18 +0000

    create database and user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,5 +7,6 @@ CREATE DATABASE
 IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER
-IF NOT EXISTS 'user_0d_2'@'hbtn_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
-
+IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON `hbtn_0d_2`.* TO 'user_0d_2@localhost';
+FLUSH PRIVILEGES;
