3cd17cf7b5544a528a1c53590740bd9cb0a5a949
inoublii <inoublii@gmail.com>
2020-03-29 23:22:15 +0100

    permissions

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,10 +11,9 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    e = 'mysql+mysqldb://{}:{}@localhost/{}'.format
-	(argv[1],
-    argv[2],
-    argv[3])
+    e = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+													argv[2],
+													argv[3])
     engine = create_engine(e)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
