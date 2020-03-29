fe50411bfc92a7319c9ed66f9a1607b9dd0cc4c6
inoublii <inoublii@gmail.com>
2020-03-29 21:05:15 +0100

    safe from sqlinjection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that takes in arguments and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections!.
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1],
+        passwd=argv[2], db=argv[3])
+    word = MySQLdb.escape_string(argv[4]).decode()
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states WHERE name LIKE '{}%'\
+         ORDER BY id".format(word))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    db.close()
+    cur.close()
