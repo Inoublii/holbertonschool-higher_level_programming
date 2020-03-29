29278954d09077d574cbde49085051a7f6998fbd
inoublii <inoublii@gmail.com>
2020-03-29 21:20:06 +0100

    inner join

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost", user=argv[1],
         passwd=argv[2], port=3306, db=argv[3])
-    word = MySQLdb.escape_string(argv[4]).decode()
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name LIKE '{}%'\
-         ORDER BY id".format(word))
+    cur.execute("SELECT cities.id, cities.name, states.name\
+         FROM states INNER JOIN cities ON \
+             states.id=cities.state_id ORDER BY cities.id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
