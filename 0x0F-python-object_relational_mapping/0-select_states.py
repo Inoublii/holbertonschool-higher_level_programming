4dfb8ec9ef2dc7d124e4f1aea88e5e13c5a5daa7
inoublii <inoublii@gmail.com>
2020-03-29 22:38:42 +0100

    permissions

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         host="localhost", user=argv[1],
         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id LIMIT 5")
+    cur.execute("SELECT id, name FROM states ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
