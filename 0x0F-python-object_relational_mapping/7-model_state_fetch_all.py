7e021b3bd5add31d7a553d3f5b3797cbbe88ae76
inoublii <inoublii@gmail.com>
2020-03-29 22:27:47 +0100

    object rational mapping

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects
+from the database hbtn_0e_6_usa
+"""
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    e = 'mysql+mysqldb://{}:{}@localhost/{}'.format
+	(argv[1],
+    argv[2],
+    argv[3])
+    engine = create_engine(e)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    for state in s.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+    s.close()
