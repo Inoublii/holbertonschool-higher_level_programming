1642bebf8a2a94f597067f27c2d3b627874c9a01
inoublii <inoublii@gmail.com>
2020-03-29 20:21:11 +0100

    connect to database and select id name

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,8 +3,9 @@
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1],
+        passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states ORDER BY id")
