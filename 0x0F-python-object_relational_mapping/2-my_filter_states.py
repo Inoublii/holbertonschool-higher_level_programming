e10909721fbaa1323864b93204c49c3861786237
inoublii <inoublii@gmail.com>
2020-03-29 20:53:29 +0100

    display name of argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1],
+        passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states WHERE name LIKE '{}%'\
+         ORDER BY id".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    db.close()
+    cur.close()
