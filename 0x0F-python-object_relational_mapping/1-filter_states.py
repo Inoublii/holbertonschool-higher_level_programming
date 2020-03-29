e3493725e4ea25ac9bccf3092cac6ae44a113cdc
inoublii <inoublii@gmail.com>
2020-03-29 22:44:45 +0100

    permissions

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         host="localhost", user=argv[1],
         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT *, name FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
