f9cd3581749200d3ce1f43ac4def4d561d09c672
inoublii <inoublii@gmail.com>
2020-05-05 22:56:21 +0100

    semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 const len = process.argv.length;
 if (len < 3) {
   console.log('No argument');
+} else if (len === 3) {
+  console.log('No argument');
 } else {
   console.log('Arguments found');
 }
