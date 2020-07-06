c7d589379052f0d4c79688ec41b448e468ef936e
inoublii <inoublii@gmail.com>
2020-07-06 14:21:04 +0100

    fx

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (h > 0 && w > 0) {
-      this.width = w;
-      this.height = h;
-    }
+	constructor (w, h) {
+	  this.width = w;
+	  this.height = h;
+	}
   }
-}
-module.exports = Rectangle;
+  module.exports = Rectangle;
